209496ac3ac2c2aeb504553f1f468a82ff017005
iciac <47710784+iciac@users.noreply.github.com>
2022-01-31 17:43:36 +1030

    Update keras_numpy_module.py

--- concrete/keras/keras_numpy_module.py
+++ concrete/keras/keras_numpy_module.py
@@ -1,6 +1,6 @@
+
 """ A keras to numpy module. """ 
 
-import numpy 
 import tensorflow.keras as keras 
 import tensorflow.keras.layers as layers 
 import tensorflow.keras.activations as activations 
@@ -21,14 +21,13 @@ class KerasNumpyModule:
 			keras_model : A fully trained, keras model along with its parameters 
 		""" 
 		self.keras_model = keras_model 
-		self.keras_model_dict = None 
 		self.check_compatibility() 
 		self.convert_to_numpy() 
 
 	def check_compatibility(self): 
 		"""Check the compatibility of all layers in the keras model.""" 
 
-		for layer in self.keras_model.layers : ### : 
+		for layer in self.keras_model.layers : 
 			if (layer_type := type(layer)) not in self.IMPLEMENTED_MODULES:
 				raise ValueError(
 					f"The following module is currently not implemented: {layer_type.__name__}. "
@@ -37,33 +36,32 @@ class KerasNumpyModule:
 				)
 		return True 
 
-	def get_model_weights(self): 
-		""" Get the Keras model weights in a dictionary. """ 
-
-		model_dict = {layer.name: [layer.get_weights(),layer.activation.__name__, type(layer)] for layer in model.layers
-		if hasattr(layer,'activation')}		
-		return model_dict 
-
-	#def get_layer_activations(self): 
-	#	for layer in self.keras_model.layers: 
-
 
 	def convert_to_numpy(self): 
 		""" Transform all parameters from keras tensors to numpy arrays. """ 
 
 		self.numpy_module_dict = {} 
-		self.keras_model_dict = self.get_model_weights() 
 
-		for name in self.keras_model_dict.keys(): 
-			weights, bias, activation, layer_type = self.keras_model_dict[name][0][0], self.keras_model_dict[name][0][1], self.keras_model_dict[name][1], self.keras_model_dict[name][2]
+		for layer in self.keras_model.layers: 
+			name = layer.name  
+			layer_type = type(layer) 
+			if hasattr(layer, "weights"): 
+				if layer.weights != []:  
+					weights = layer.weights[0] 
+					bias = layer.weights[1] 
+
+				else: 
+					weights = None 
+					bias = None 
+			if hasattr(layer, "activation"): 
+				activation = layer.activation.__name__  
+			else: 
+				activation = None 
+
 			self.numpy_module_dict[name] = {"type": layer_type,
-											"weights":weights, # may need transpose? 
+											"weights":weights, 
 											"bias": bias, 
 											"activation": activation}  
-			#self.numpy_module_dict[str(name)+"weight"] = weights.T
-			#self.numpy_module_dict[str(name)+"bias"] = bias 
-			#self.numpy_module_dict[str(name)+"activation"] = activation 
-
 
 
 	def __call__(self, x: numpy.ndarray): 
@@ -81,16 +79,25 @@ class KerasNumpyModule:
 
 		""" 
 
-		#for name, layer in A: 
-		#for layer in self.keras_model_dict: 
-		#	x = ( x @ self.)
-
 		for layer_name in self.numpy_module_dict: 
 			if self.numpy_module_dict[layer_name]["type"] == layers.Dense: 
+
 				x = (x @ self.numpy_module_dict[layer_name]["weights"] + self.numpy_module_dict[layer_name]["bias"]) 
+
 				if self.numpy_module_dict[layer_name]["activation"] == "relu": 
-					x = numpy.minimum(numpy.maximum(0, x), 6) # ReLU 6 
-				elif self.numpy_module_dict[layer_name]["activation"] == "softmax": 
+					x = numpy.maximum(0, x) 
+
+				if self.numpy_module_dict[layer_name]["activation"] == "relu6": 
+					x = numpy.minimum(numpy.maximum(0,x),6) 
+
+				elif self.numpy_module_dict[layer_name]["activation"] == "sigmoid": 
 					x = 1 / (1 + numpy.exp(-x))
+
+				elif self.numpy_module_dict[layer_name]["activation"] == "softmax": 
+					x = numpy.exp(x) / numpy.sum(numpy.exp(x)) 
+
+			elif self.numpy_module_dict[layer_name]["type"] == layers.Flatten: 
+				x = x.flatten() 
+				x = x.reshape(1, x.shape[0])
 
 		return x 
